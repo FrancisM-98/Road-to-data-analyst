8ae43fa8f350b5c28eced421b15cf244a5cd2aa5
FrancisM-98 <multanifrancis@gmail.com>
2026-01-22 11:55:34 +0100

    Name was strange

--- Codédex/Final project/DevineLeNombre.py
+++ Codédex/Final project/DevineLeNombre.py
@@ -7,3 +7,10 @@ theme_choisi = random.choice(themes)
 print(f"Thème attribué : {theme_choisi}, Note attribuée : {note}")
 print("Faites deviner le thème attribué en donnant un mot du thème qui correspondrait le mieux à la note attribuée.")
 
+# Website for the Final project
+import streamlit as st
+import random
+st.title("Devine la note du thème !")
+st.write("Bienvenue dans le jeu Devine la note du thème !")
+st.write("L'objectif est de deviner la note attribuée à un thème donné en proposant des mots associés à ce thème.")
+st.button("Commencer une nouvelle partie")
